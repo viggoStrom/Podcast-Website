feefd4a714edf01f7a858a1a757704ca230fe2a4
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-02-19 15:01:25 +0100

    custom audio player

--- script/playHandler.js
+++ script/playHandler.js
@@ -1,10 +1,4 @@
-const back = document.querySelector("#episodes ul li #textWrapper #customPlayer p:nth-of-type(1)")
-const pause = document.querySelector("#episodes ul li #textWrapper #customPlayer p:nth-of-type(2)")
-const forward = document.querySelector("#episodes ul li #textWrapper #customPlayer p:nth-of-type(3)")
-const bar = document.querySelector("#episodes ul li #textWrapper #customPlayer div")
-const time = document.querySelector("#episodes ul li #textWrapper #customPlayer p:nth-of-type(4)")
-
-console.log(bar.children[0].style.flex = 9);
-console.log(bar.children[1].style.flex = 1);
-
-time.innerHTML = "12:34"
+// lists of iteractable elements so you can iterate through
+// pause other podcast when you start another one
+// having every button call the same function and check class (className) for what to do
+// .oninput for slider
